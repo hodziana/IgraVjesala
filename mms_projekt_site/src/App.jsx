8a74e4dde2ba429331886bd37003366c1eb9066e
Mario Borna Mjertan <bor.mbm@gmail.com>
2025-08-28 23:54:41 +0200

    Add features section

--- mms_projekt_site/src/App.jsx
+++ mms_projekt_site/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Hero from './Hero'
+import Features from './Features'
 
 function App() {
 
   return (
     <>
       <Hero></Hero>
+      <Features></Features>
     </>
   )
 }
